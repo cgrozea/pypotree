194a21dce7437448b9521a2c6aca08b08b203a4f
C.G. <cgrozea@users.noreply.github.com>
2025-06-26 11:08:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ requirements = [ 'numpy',
 
 
 setup(name="pypotree",
-      version="1.0.5-jupyterhub",
+      version="1.0.5.post1",
       description='Potree visualization for jypyter notebooks',
       long_description=readme(),
       long_description_content_type='text/markdown',
